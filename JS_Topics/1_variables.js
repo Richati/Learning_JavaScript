d4aca66aaa4982e2151df3703a54b6817d14d282
Richa Tiwari <Richa.10725493@ltimindtree.com>
2023-08-08 16:24:30 +0530

    variable type and msg box is completed

---
+++ JS_Topics/1_variables.js
@@ -0,0 +1,52 @@
+
+//var declaration 
+let message;
+//var initailization 
+message = "hello , how are you?"
+
+console.log(message);
+
+let message1= "hi i am variable code";
+console.log(message1);
+
+let a=12,b=13,c=22;
+console.log(a);
+console.log(b);
+console.log(c);
+
+message = "this  modified code";
+console.log(message);
+
+//creating const variable
+
+const  user = "codeWithRicha";
+console.log(user);
+if(5>2){
+    console.log("condition true ");
+    let d=10;
+    console.log(d);
+   // let city = "delhi";
+  var city1= "hyd";
+}
+//console.log(d);
+//console.log(city);
+console.log(city1);
+
+var x=10;
+var x=11;
+console.log(x);
+
+//naming convention
+
+let ABC=12;
+console.log(ABC);
+
+let aBC=13;
+console.log(aBC);
+let _22="rr";
+console.log(_22);
+
+let _name="richa" ;
+console.log(_name);
+let Const =12;
+console.log(Const);
