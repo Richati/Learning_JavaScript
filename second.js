c2190506b8522db8a5e5857fb7f398d0fc07a0e9
Richa Tiwari <Richa.10725493@ltimindtree.com>
2023-08-08 12:37:07 +0530

    second javascript code committed

---
+++ second.js
@@ -0,0 +1,7 @@
+//this is javascript code
+//we can execute nodejs code
+console.log("this is second javascript file");
+var x=23;
+var y=23;
+console.log("sum of x and y is  " + (x+y));
+console.log("THANKS & Bye Bye");
